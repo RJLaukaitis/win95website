cdd3a2728f0baee549c6e5f7aa147570d9918cc5
Ronald Laukaitis <rol226@lehigh.edu>
2024-04-22 09:48:23 -0400

    Added contact page and fixed the vertical menu to have asterisl indicator for current page

---
+++ src/components/Portfolio/Contact.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import './Contact.css';
+import VerticalMenu from "../VerticalMenu/verticalmenu";
+import emailjs from 'emailjs-com';
+
+const Contact = () => {
+    // State hooks to keep track of input values and submission status
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        company: '',
+        message: ''
+    });
+    const [emailSent, setEmailSent] = useState(false);
+
+    // Update form data as the user types
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    // Handle form submission
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        emailjs.sendForm('service_p87humi', 'template_9ohnrxw', event.target, 'kEfKQoxB0-m0FkKJp')
+        .then((result) => {
+            console.log('Email successfully sent!', result.text);
+            setFormData({name: '', email: '', company: '', message: ''}); // Clear form
+            setEmailSent(true); // Indicate email was sent successfully
+        }, (error) => {
+            console.log('Failed to send email.', error.text);
+            setEmailSent(false); // Optionally handle error state
+        });
+    };
+
+    return (
+        <div className="contact-container">
+            <VerticalMenu/>
+            <div className="contact-content">
+                <h1 className="ContactHeader">
+                    Contact
+                </h1>
+                <p className="introduction">
+                Please feel free to reach out using the form below - All inquiries are forwarded to my personal email address. You can also use my personal email address below if you'd like to send it through a web/desktop email client.
+                </p>
+                <div className="PersonalEmail">
+                Email Address: <span className="EmailLink">Rjlaukaitis@gmail.com</span>
+                </div>
+                <hr className="solid"></hr>
+                {emailSent && <div className="success-message">Your message has been sent successfully!</div>}
+                <form onSubmit={handleSubmit} className="contact-form">
+                    <label htmlFor="name"><span className="required-asterisk">*</span> Your name:</label>
+                    <input id="name" type="text" name="name" value={formData.name} onChange={handleChange} required />
+
+                    <label htmlFor="email"><span className="required-asterisk">*</span>  Email:</label>
+                    <input id="email" type="email" name="email" value={formData.email} onChange={handleChange} required />
+
+                    <label htmlFor="company">Company (optional):</label>
+                    <input id="company" type="text" name="company" value={formData.company} onChange={handleChange} />
+
+                    <label htmlFor="message"><span className="required-asterisk">*</span> Message:</label>
+                    <textarea id="message" name="message" value={formData.message} onChange={handleChange} required></textarea>
+
+                    <button type="submit" className="send-message-button">Send Message</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
