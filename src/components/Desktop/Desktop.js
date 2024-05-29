baa80454de22ecd18ac07dbd16bcaf5456718c1b
Ronald Laukaitis <rol226@lehigh.edu>
2024-05-28 20:05:34 -0700

    Added mouseclick sound and ambience

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -1,22 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigator from '../PortfolioNavigator/Navigator.js';
 import Icon from "../Icons/Icon.js";
 import Draggable from 'react-draggable';
 import { Resizable, ResizableBox } from 'react-resizable';
+import pcNoise from "../../Audio/Ambience.mp3";
+import clickNoise from "../../Audio/MouseClick.wav";
 import '98.css';
 import './Desktop.css'; // Ensure styles do not conflict with 98.css
 import 'react-resizable/css/styles.css'; // Import CSS for resizable component
 
 import PortfolioIcon from "../../images/DocumentIcon.png";
 import SolitaireIcon from "../../images/solitaireIcon.png";
-import CardIcon from "../../images/mailbox.png";
-import CardVideo from "../../Videos/Card.mp4";
 
 function Desktop() {
     const [isSolitaireOpen, setIsSolitaireOpen] = useState(false);
     const [isPortfolioOpen, setIsPortfolioOpen] = useState(false);
     const [isCardOpen, setIsCardOpen] = useState(false);
     const [isDragging, setIsDragging] = useState(false);
+
+    useEffect(() => {
+        const playClickSound = () => {
+            var clickSound = new Audio(clickNoise);
+            clickSound.volume =0.15;
+            clickSound.play();
+        };
+
+        // Play ambient sound
+        const ambience = new Audio(pcNoise);
+        ambience.loop = true;
+        ambience.volume = 0.1;
+        //ambience.play();
+
+        window.addEventListener('mousedown', playClickSound);
+
+        return () => {
+            window.removeEventListener('mousedown', playClickSound);
+        };
+    }, []);
 
     return (
         <div className="desktop">
@@ -59,36 +79,13 @@ function Desktop() {
                                 </div>
                             </div>
                             <div className="window-body">
-                                <iframe src="Solitaire/index.html" title="Solitaire" style={{ width: '50vw', height: '50vh', border: 'none' }}></iframe>
+                                <iframe src="../../Solitaire/src/index.js" title="Solitaire" style={{ width: '50vw', height: '50vh', border: 'none' }}></iframe>
                             </div>
                         </div>
                 </Draggable>
             )}
-            {/* <Icon label="Businesscard" imgSrc={CardIcon} onClick={() => setIsCardOpen(true)} />
-            {isCardOpen && (
-                <Draggable handle='.title-bar'
-                onStart={()=> setIsDragging(true)}
-                onStop={()=> setIsDragging(false)}
-                defaultPosition={{x:300,y:100}}
-                >
-                    <div className={`window ${isDragging ? 'dragging' : ''}`}>
-                        <div className="title-bar">
-                            <div className="title-bar-text">bus.card</div>
-                            <div className="title-bar-controls">
-                                    <button aria-label="Minimize" />
-                                    <button aria-label="Maximize" />
-                                    <button aria-label="Close" onClick={() => setIsCardOpen(false)} />
-                            </div>
-                        </div>
-                        <div className="window-body">
-                            <video muted className="CardVideo" height="500" width ="750" autoplay >
-                                <source src={CardVideo} type="video/mp4"></source>
-                            </video>
-                        </div>
-                    </div>
-                </Draggable>
-            )} */}
         </div>
+
     );
 }
 
