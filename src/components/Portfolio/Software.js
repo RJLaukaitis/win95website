167dfe57b07eb6ab9d2323c2f1be2ad3d955a780
Ronald Laukaitis <rol226@lehigh.edu>
2024-07-01 23:44:45 -0700

    Fixed captions and fonts

--- src/components/Portfolio/Software.js
+++ src/components/Portfolio/Software.js
@@ -24,7 +24,7 @@ const Software = () => {
                 <h1>alchemy </h1>
                 <h2>June 2024</h2>
                         <p>
-                        Alchemy is a highly customizable solana based cryptocurrency trading bot designed to provide users with a competitive advantage in trading the latest coins and tokens. The bot holds an array of powerful modules, including advanced Twitter and Telegram monitoring for coin releases and drops. Additionally, Alchemy features a sophisticated PumpdotFun screener, utilizing an algorithm to rate and score coins based on multiple metrics.   
+                        Alchemy is a highly customizable solana based cryptocurrency trading bot designed to provide users with a competitive advantage in trading the latest coins and tokens. The bot holds an array of powerful modules, including advanced Twitter and Telegram monitoring for coin releases and drops. There's also a sophisticated PumpdotFun screener, utilizing a proprietary  algorithm to rate and score coins based on multiple metrics.   
                         </p>
                         <div className="alchemyHome">
                         <div className="Media">
@@ -36,12 +36,16 @@ const Software = () => {
                         </div>
                         </div>
                         <div className="captions">
-                        <p>
-                            Figure 1: Alchemy home screen.                                                                               
-                        </p>
+                            <p>
+                            <span>Figure 1: Alchemy home screen.  </span>
+                            <span>Figure 2: Demo of the Telegram module</span>        
+                            </p>
                         </div>
                         <p>
                         I built Alchemy in June of 2024, after solana memecoins started taking over my twitter feed. Noticing the lucrative opportunity some of these coins could offer, I started planning out what tools would be most useful for being on the winning side of trades. It took a lot of research and experimentation with different API's and services, however, I was able to develop a system that could reliably monitor and evaluate coins in real time. The result was a robust and reliable trading bot that allows users to capitalize on emerging trends in the cryptocurrency market.
+                        </p>
+                        <p>
+                        The github repo is not public to avoid plagiarism, however if you wish to see the source code please feel free to contact me.
                         </p>
                         <div className="features">
                             <h3> Features:</h3>
