f0707f9d69f1628a8747b230606a8b3a88e89bc4
Ronald Laukaitis <rol226@lehigh.edu>
2024-07-03 15:28:56 -0700

    Fixed portfolio window location

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -87,7 +87,7 @@ function Desktop() {
                 <Draggable handle=".title-bar"
                     onStart={()=> setIsDragging(true)}
                     onStop={()=> setIsDragging(false)}
-                    defaultPosition={{x: 50, y: 10}}
+                    defaultPosition={{x: 100, y: 10}}
                 >       
                         <div className={`window ${isDragging ? 'dragging' : ''}`}>
                             <div className="title-bar">
--- src/components/StartMenu/StartMenu.js
+++ src/components/StartMenu/StartMenu.js
@@ -1,32 +1,34 @@
-import react from 'react';
+import React from 'react';
 import './StartMenu.css';
-import  Icon from '../Icons/Icon.js'
+import Icon from '../Icons/Icon.js';
 import ShutDownIcon from "../../images/ShutDownIcon.png";
+import errorSound from "../../Audio/Error.mp3"; // Adjust the path as necessary
 
-function StartMenu(){
+function StartMenu() {
+    const error = new Audio(errorSound);
+
     const handleShutdown = () => {
-        /*Add shutdown logic */
-        console.log('Shutting down');
+        error.play();
+        // Add additional shutdown logic here
     };
 
-return (
-    <div className="start-menu">
-        <div className="Laukaitis-text">
-            Laukaitis
+    return (
+        <div className="start-menu">
+            <div className="Laukaitis-text">
+                Laukaitis
+            </div>
+            <div className="OS-text">
+                OS
+            </div>
+            <div className="shutdownIcon" onClick={handleShutdown}>
+                <img src={ShutDownIcon} alt='Shutdown'/>
+                <span className='Shutdown-text'>
+                    Shutdown
+                </span>
+            </div>
+            {/* Add other menu items here */}
         </div>
-        <div className="OS-text">
-            OS
-        </div>
-
-        <div className='shutdownIcon'>
-            <img src={ShutDownIcon} alt='Shutdown'/>
-            <span className='Shutdown-text'>
-            Shutdown
-        </span>
-        </div>
-        {/*Add other menu items here */}
-    </div>
-);
+    );
 }
 
 export default StartMenu;
