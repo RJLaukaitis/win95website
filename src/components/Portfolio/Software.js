d4ed7aaad95612122760eb65cfdd3aedb7738f3f
Ronald Laukaitis <rol226@lehigh.edu>
2024-08-06 22:35:21 -0700

    Added more to software

--- src/components/Portfolio/Software.js
+++ src/components/Portfolio/Software.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import './Software.css';
 import VerticalMenu from '../VerticalMenu/verticalmenu';
 import alchemyHome from "../../images/AlchemHome.png"
-import Gargantua from "../../Videos/Gargantua.mp4";
+import alchemyDemo from "../../Videos/AlchemyDemo.mp4";
 import draft from "../../images/websiteDraft.png";
 import final from "../../images/final.png";
 
@@ -25,23 +25,29 @@ const Software = () => {
             <hr class="solid"></hr> 
             <div className="website">
                 <h1>RonaldLaukaitis.com </h1>
-                <h2>April-August 2024</h2>
+                <h2>April 2024 - August 2024</h2>
+                    <div className="paragraph">
                         <p>
                         I always wanted to create my own personal portfolio website and I became inspired after seeing Henry Heffernan's 2022 showcase, which can be found <a href= "https://henryheffernan.com/">here</a>. I decided to follow his same framework and started working on the windowsOS portion in April of 2024. This was my first time delving into JS and CSS so it took some time getting adjusted however I picked it up pretty quick and was able to create the desktop page in a single sitting. Once I had this framework in place I started working on the actual portfolio page and chipped away at it during the rest of my school year.
                         </p>
+                        </div>
                         <div className="Media1">
                         <img src = {draft} alt="" />
                         <img src = {final}></img>
                         </div>
-                        <div className="caption1">
+                        <div className="webCaption">
                             <p>
-                                Figure 1: Comparison between first iteration and final iteration of the outer-site.
+                                Figure 1: Comparison between the first and final iteration of the office space.
                             </p>
                         </div>
+                        <div className="paragraph">
                         <p>
                             The 3d portion of the website was created using THREEjs and blender. Once i had the office scene laid out, I baked the lighting from blender into individual objects' textures giving everything a realistic and polished look. Having never worked with THREEjs before it posed quite a challenge to initially begin, however after a couple weeks of trial and error, I was able to get my model displaying properly and had the foundation set.
-                
                         </p>
+                        <p>
+                            Once the summer rolled around I could work much more often and push through the late stages of development with bug fixes and small tweaks. I'm so proud with how the end result turned out and working on this really grew my skills as a programmer and how I approach a challenge.
+                        </p>
+                        </div>
             </div>
 
             <hr class="solid"></hr> 
@@ -52,25 +58,25 @@ const Software = () => {
                         Alchemy is a highly customizable solana based cryptocurrency trading bot designed to provide users with a competitive advantage in trading the latest coins and tokens. The bot holds an array of powerful modules, including advanced Twitter and Telegram monitoring for coin releases and drops. There's also a sophisticated PumpdotFun screener, utilizing a proprietary  algorithm to rate and score coins based on multiple metrics.   
                         </p>
                         <div className="alchemyHome">
-                        <div className="Media">
-                        <img src = {alchemyHome} alt="" />
-                        <video className="video1" width="500" height="300" autoplay controls muted loop>
-                        <source src={Gargantua} type="video/mp4"></source>
+                        <div className="mediaContainer">
+                        <img src = {alchemyHome} alt="Alchemy home" className="mediaImage"/>
+                        <video autoplay controls muted loop className="mediaVideo">
+                            <source src={alchemyDemo} type="video/mp4" />
                         </video>
 
                         </div>
                         </div>
                         <div className="captions">
                             <p>
-                            <span>Figure 1: Alchemy home screen.  </span>
-                            <span>Figure 2: Demo of the Telegram module</span>        
+                            <span>Figure 2: Alchemy home screen.  </span>
+                            <span>Figure 3: Demo of the Telegram module</span>        
                             </p>
                         </div>
                         <p>
                         I built Alchemy in June of 2024, after solana memecoins started taking over my twitter feed. Noticing the lucrative opportunity some of these coins could offer, I started planning out what tools would be most useful for ending up on the winning side of trades. It took a lot of research and experimentation with different API's and services, however, I was able to develop a system that could reliably monitor and evaluate coins in real time. The result was a robust and reliable trading bot that allows users to capitalize on emerging trends in the cryptocurrency market.
                         </p>
                         <p>
-                        The github repo is not public to avoid my code being used elsewhere, however if you wish to see parts of source code please feel free to contact me.
+                        The github repo is not public as of due to there being sensitive data, however if you wish to see parts of the source code please feel free to contact me.
                         </p>
                         <div className="features">
                             <h3> Features:</h3>
@@ -96,7 +102,7 @@ const Software = () => {
             <h1>T-Recs</h1>
                 <h2>June 2024 - August 2024</h2>
                 <p>
-                    T-recs is a web based app to help Lehigh faculty and admins track student integrity violations across departments. Faculty are able to securely log into the platform through their Lehigh email accounts, and submit a report form about the incident. I worked on this app with a few other students from the University, our professor, Mark Erle reached out to students for summer research opportunities and I jumped in as soon as i could. Our team of five split-up to work in the sections we had the most experience in. I started working on the backend components, and as of writing, the project is on-going.
+                    T-recs is a web based app to help Lehigh faculty and admins track student integrity violations across departments. Faculty are able to securely log into the platform through their Lehigh email accounts, and submit a report form about the incident. I worked on this app with a few other students from the University, our professor reached out to students for summer research opportunities and I jumped in as soon as I could. Our team of five split-up to work in the sections we had the most experience in. I started working on the backend components, and as of writing, the project is on-going.
                 </p>
         
                 </div>
